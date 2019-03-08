1c0aee7af6e794542c370a207febbc50b192df72
Harshit <agrawalharshit16@gmail.com>
2019-03-09 05:04:25 +0530

    adding qr scan and manual entry

--- components/VisitorVerification/index.js
+++ components/VisitorVerification/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, TextInput, Animated } from 'react-native';
+import { View, Text, TextInput, Animated } from 'react-native';
 import QRCodeScan from '../QRCodeScanner';
-import SlidingUpPanel from 'rn-sliding-up-panel';
 import { styles } from './style';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 class VisitorVerification extends Component {
     static navigationOptions = {
@@ -14,11 +14,6 @@ class VisitorVerification extends Component {
         animatedValue: new Animated.Value(0)
     }
 
-    showPanel = () => {
-        this.input.focus();
-        this.panel.show();    
-    }
-
     handleChange = (text) => {
         this.setState({
             qrCode: text
@@ -26,26 +21,12 @@ class VisitorVerification extends Component {
     }
 
     render() {
+        console.log(this.state)
         return (
             <View style={{flex:1,justifyContent: 'center'}}>
                 <QRCodeScan 
-                    showPanel={this.showPanel} 
                     handleChange={this.handleChange}
                 />
-                <SlidingUpPanel 
-                    ref={node => this.panel = node}
-                    draggableRange={{top:500,bottom:0}}
-                    animatedValue={this.state.animatedValue}
-                >
-                    <View style={styles.panelView}>
-                        <TextInput
-                            ref={node => this.input = node} 
-                            placeholder="Enter Code"
-                            onChangeText={this.handleChange}
-                            style={styles.input}
-                        />
-                    </View>
-                </SlidingUpPanel>
             </View>
         );
     }
